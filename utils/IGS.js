f5520e883f3822250cd31e1623f3fc87f1e6bb92
Zhirrr <75620279+Zhirrr@users.noreply.github.com>
2021-02-07 16:13:25 +0700

    Update IGS.js

--- utils/IGS.js
+++ utils/IGS.js
@@ -24,20 +24,7 @@ const IGS = (user) => new Promise((resolve, reject) => {
 		ig.getUserMeta(user, options).then(data => {
 			resolve({
 				code:200,
-				data: {
-					profile: data.graphql.user.profile_pic_url,
-					profilehd: data.graphql.user.profile_pic_url_hd,
-					fullname: data.graphql.user.full_name,
-					private: data.graphql.user.is_private,
-					verified: data.graphql.user.is_verified,
-					bio: data.graphql.user.biography,
-					follower: data.graphql.user.edge_followed_by.count,
-					following: data.graphql.user.edge_follow.count,
-					conneted_fb: data.graphql.user.connected_fb_page,
-					videotimeline: data.graphql.user.edge_felix_video_timeline.count,
-					timeline: data.graphql.user.edge_owner_to_timeline_media.count,
-					savedmedia: data.graphql.user.edge_saved_media.count,
-					collections: data.graphql.user.edge_media_collections.count
+				data
 				}
 			})
 		}).catch(err => reject("Username Not Found"))
