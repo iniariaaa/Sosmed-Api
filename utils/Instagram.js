badd041d45219e5eb4eaab6d888bcf7f61e03a56
Zhirrr <75620279+Zhirrr@users.noreply.github.com>
2021-02-07 13:36:36 +0700

    Update Instagram.js

--- utils/Instagram.js
+++ utils/Instagram.js
@@ -3,7 +3,7 @@ const baseURI = 'https://api.arugaz.my.id'
 
 module.exports.Instagram = function Instagram(url) {
 	return new Promise((resolve, reject) => {
-		fetch(baseURI+'/api/media/ig?url=' + query)
+		fetch(baseURI+'/api/media/ig?url=' + url)
 		.then(res => res.json())
 		.then(resolve)
 		.catch(reject)
